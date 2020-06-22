6a92b26e6e26214f32902b99ee8d8c1a4e872721
Nhan1506 <lehuunhan150698@gmail.com>
2020-06-21 23:53:01 -0400

    employee update

--- models/accountModel.js
+++ models/accountModel.js
@@ -8,18 +8,18 @@ module.exports = {
   findOne: (field, value) => {
     return db.load(`select * from account where ${field} = '${value}'`);
   },
-<<<<<<< HEAD
+
   findCustom: (field) =>{
     return db.load(`select ${field} from account`);
   },
   findByUserId: (id) => db.load(`select * from account where id = ${id}`),
-=======
+
   findUserIdByAccountNumber: () => {
     return db.load(`select user from account where checking_account_number = ${checking_account_number} 
           
     `);
   },
->>>>>>> fa62c0519a1043c88ab43e99c7b5fca374813b7d
+
   updateCheckingMoney: async (checking_account_number, amount) => {
     const account = await db.load(
       `select * from account where checking_account_number = ${checking_account_number}`
