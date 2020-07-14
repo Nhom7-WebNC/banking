2537a58709f8a4728eda0175a478d2fe6470f9f2
PhongLeDev <phongcoi696@gmail.com>
2020-07-14 10:09:20 +0700

    hi

--- models/receiverModel.js
+++ models/receiverModel.js
@@ -7,6 +7,9 @@ module.exports = {
   findByAccountNumber: (receiver_account_number) =>
     db.load(`select * from receiver_list where receiver_account_number = ${receiver_account_number}`),
   findByUserId: (id) => db.load(`select  * from receiver_list where user_id = ${id}`),
-  findByOtherName: (other_name) =>
-    db.load(`select  * from receiver_list where receiver_other_name = ${other_name}`),
+  findByUserIdAndBankCode: (user_id, bank_code) =>
+    db.load(`select  * from receiver_list where user_id = ${user_id} and bank_code = '${bank_code}'`),
+  findOne: (field, value) => {
+    return db.load(`select * from account where ${field} = '${value}'`);
+  },
 };
