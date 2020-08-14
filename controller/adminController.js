09aad7b4b678228e4bb55ea81668ff8c57de4499
phong1998 <ngphong11998@gmail.com>
2020-08-14 18:00:23 +0700

    admin history transaction

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const transactionModel = require("../models/transactionModel");
 module.exports = {
   manager: async function (req, res, next) {
     await userModel.findOne("role_name", "employee").then((rows) => {
@@ -10,60 +11,60 @@ module.exports = {
   createAccount: async function (req, res, next) {
     const password = req.body.password;
     console.log(password);
-    const user = userModel.findOne("username", req.body.username).then((rows) => {
-      if (rows.length > 0) {
-        res.status(403).json({ msg: "tai khoan da ton tai" });
-      } else {
-        if (!req.body.username) {
-          res.status(403).json({ msg: "khong co username" });
-        }
+    const user = userModel
+      .findOne("username", req.body.username)
+      .then((rows) => {
+        if (rows.length > 0) {
+          res.status(403).json({ msg: "tai khoan da ton tai" });
+        } else {
+          if (!req.body.username) {
+            res.status(403).json({ msg: "khong co username" });
+          }
 
-        var code;
-        bcrypt.genSalt(10, async (err, salt) => {
-          bcrypt.hash(password, salt, function (err, hash) {
-            passwordHash = hash;
+          var code;
+          bcrypt.genSalt(10, async (err, salt) => {
+            bcrypt.hash(password, salt, function (err, hash) {
+              passwordHash = hash;
 
-            const newUserMysql = {
-              username: req.body.username,
-              password: passwordHash,
-              name: req.body.name,
-              phone_number: req.body.phone_number,
-              email: req.body.email,
-              birthday: req.body.birthday,
-              address: req.body.address,
-              gender: req.body.gender,
-              role_name: req.body.role_name,
-              personal_number: req.body.personal_number,
-            };
-            //create checking number
-            console.log(newUserMysql);
+              const newUserMysql = {
+                username: req.body.username,
+                password: passwordHash,
+                name: req.body.name,
+                phone_number: req.body.phone_number,
+                email: req.body.email,
+                birthday: req.body.birthday,
+                address: req.body.address,
+                gender: req.body.gender,
+                role_name: req.body.role_name,
+                personal_number: req.body.personal_number,
+              };
+              //create checking number
+              console.log(newUserMysql);
 
-            userModel.add(newUserMysql);
+              userModel.add(newUserMysql);
 
-            return res.status(200).json("dang ki thanh cong" + { newUserMysql });
+              return res
+                .status(200)
+                .json("dang ki thanh cong" + { newUserMysql });
+            });
           });
-        });
-      }
-    });
-  },
-  delete: async function(req,res,next){
-    
-      const entity = {
-          id: req.params.id,
-      }
-      //userModel.delete(entity)
-      await userModel.delete(entity)
-      await userModel.findOne("role_name", "employee").then((rows) => {
-       
-        res.status(200).json({ data: rows });
+        }
       });
   },
+  delete: async function (req, res, next) {
+    const entity = {
+      id: req.params.id,
+    };
+    //userModel.delete(entity)
+    await userModel.delete(entity);
+    await userModel.findOne("role_name", "employee").then((rows) => {
+      res.status(200).json({ data: rows });
+    });
+  },
   update: async function (req, res, next) {
-    
-    const id =req.body.id;
-    console.log("hihihihihi",id);
+    const id = req.body.id;
+    console.log("hihihihihi", id);
     const newUserMysql = {
-      
       username: req.body.username,
       name: req.body.name,
       phone_number: req.body.phone_number,
@@ -73,12 +74,73 @@ module.exports = {
       gender: req.body.gender,
       personal_number: req.body.personal_number,
     };
-    await userModel.updateByOne("id",id,newUserMysql);
+    await userModel.updateByOne("id", id, newUserMysql);
 
     await userModel.findOne("role_name", "employee").then((rows) => {
       console.log(rows);
       res.status(200).json({ data: rows });
     });
+  },
 
+  getTransaction_byTime: async function (req, res, next) {
+    var activeTab0 = [];
+
+    const dateStart = req.body.dateStart;
+    const dateEnd = req.body.dateEnd;
+
+    await transactionModel.findByTime(dateStart,dateEnd).then((rows) => {
+      rows.map((row) => {
+        activeTab0.push(row);
+      });
+    });
+
+    
+    activeTab0.length
+      ? res.status(200).json({
+          data: {
+            activeTab0: activeTab0,
+          },
+        })
+      : res.status(401).json({ msg: "Chưa có giao dịch" });
+
+  },
+
+  getTransaction_all: async function (req, res, next) {
+    var activeTab1 = [];
+
+    await transactionModel.findAll().then((rows) => {
+      rows.map((row) => {
+        activeTab1.push(row);
+      });
+    });
+
+    
+    activeTab1.length
+      ? res.status(200).json({
+          data: {
+            activeTab1: activeTab1,
+          },
+        })
+      : res.status(401).json({ msg: "Chưa có giao dịch" });
+
+  },
+
+  getTransaction_byBankcode: async function (req, res, next) {
+    var activeTab2 = [];
+    const bank_code = req.body.bank_code;
+
+    await transactionModel.findByBankcode(bank_code).then((rows) => {
+      rows.map((row) => {
+        activeTab2.push(row);
+      });
+    });
+
+    activeTab2.length
+      ? res.status(200).json({
+          data: {            
+            activeTab2: activeTab2,
+          },
+        })
+      : res.status(401).json({ msg: "Chưa có giao dịch" });
   },
 };
--- models/transactionModel.js
+++ models/transactionModel.js
@@ -6,11 +6,25 @@ module.exports = {
   },
   findAll: () => db.load(`select * from transaction_history`),
   findByAccountNumber: (account_number) =>
-    db.load(`select * from transaction_history where receiver_account_number = ${account_number} or sender_account_number =${account_number}` ),
-  findById: (id) => db.load(`select  * from transaction_history where receiver_id = ${id}`),
-  findByReciverAccountNumber:(account_number)=>
-  db.load(`select * from transaction_history where receiver_account_number = ${account_number}` ),
-  findBySenderAccountNumber:(account_number)=>
-  db.load(`select * from transaction_history where sender_account_number = ${account_number}` ),
-  
+    db.load(
+      `select * from transaction_history where receiver_account_number = ${account_number} or sender_account_number =${account_number}`
+    ),
+  findById: (id) =>
+    db.load(`select  * from transaction_history where receiver_id = ${id}`),
+  findByReciverAccountNumber: (account_number) =>
+    db.load(
+      `select * from transaction_history where receiver_account_number = ${account_number}`
+    ),
+  findBySenderAccountNumber: (account_number) =>
+    db.load(
+      `select * from transaction_history where sender_account_number = ${account_number}`
+    ),
+  findByBankcode: (bank_code) =>
+    db.load(
+      `select * from transaction_history where (sender_bank_code=${bank_code} or receiver_bank_code=${bank_code})`
+    ),
+  findByTime: (dateStart,dateEnd) =>
+    db.load(
+      `select * from transaction_history where created_at BETWEEN ${dateStart} AND ${dateEnd}`
+    ),
 };
